6f8c70a8cb959e531376f698aec0c9c702078766
Kushmitha Kondapalli <kondapalli.kushmitha@s.amity.edu>
2025-05-20 22:30:13 +0530

    changed a color

--- sq.py
+++ sq.py
@@ -10,7 +10,7 @@ import time #used for timer in the quiz
 colorRED = '\033[91m'
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
-CYAN = '\033[96m'
+PURPLE = '\033[96m'
 RESET = '\033[0m'
 
 def print_banner(): #Displays the welcome banner for the quiz with styled color and emoji.
