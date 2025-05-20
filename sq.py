051d9c540b8569238a4dc4ae07ae0300e4a3a53d
Kushmitha Kondapalli <kondapalli.kushmitha@s.amity.edu>
2025-05-20 21:17:25 +0530

    Add color constants for styling output

--- sq.py
+++ sq.py
@@ -3,3 +3,9 @@ def science_quiz():
     pass
 def print_banner():
     print("Welcome to the Science Quiz!")
+RED = '\033[91m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+CYAN = '\033[96m'
+RESET = '\033[0m'
+
