3ad00055b591eb4e2366860568d5878fd8772355
Kushmitha Kondapalli <kondapalli.kushmitha@s.amity.edu>
2025-05-20 22:15:25 +0530

    try1

--- sq.py
+++ sq.py
@@ -3,7 +3,7 @@ ssssssssScience Quiz Module
 Includes a multiple-choice science quiz game with scoring and replay.
 """
 
-import time #used for timer in the quiz
+iiiiiiiimport time #used for timer in the quiz
 
 # Adding colors for styling
 RED = '\033[91m'
